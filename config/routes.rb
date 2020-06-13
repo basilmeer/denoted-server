4923672d96a96a5a2b70df2e2b5fc181a445efa3
Basil Meer <basilmeer@gmail.com>
2020-06-13 17:04:43 +0500

    Setting up Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'home#index'
+
+  authenticated :user do
+    resource :notes, only: %w[new] do
+      post :new
+    end
+  end
 end
